31d99ea64d15091dc04e1a2a038234c71abb0271
Eshaan Mittal <eshaan.mittal1@gmail.com>
2022-12-05 11:47:37 +0100

    1

--- src/DiesSetmanaFitxer.java
+++ src/DiesSetmanaFitxer.java
@@ -1,2 +1,18 @@
+import java.io.*;
+import java.util.*;
+
 public class DiesSetmanaFitxer {
+    public static void main(String[] args) throws IOException {
+        try {
+            FileReader fr = new FileReader("Dies");
+            int data = fr.read();
+            while (data != -1) {
+                System.out.println(data);
+                data = fr.read();
+                fr.close();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+    }
 }
