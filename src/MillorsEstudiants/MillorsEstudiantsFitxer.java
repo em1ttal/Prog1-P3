726433ec9abac8c1307e32d0287c67caa75dfda4
Eshaan Mittal <eshaan.mittal1@gmail.com>
2022-12-18 11:44:55 +0100

    8

---
+++ src/MillorsEstudiants/MillorsEstudiantsFitxer.java
@@ -0,0 +1,12 @@
+package MillorsEstudiants;
+
+public class MillorsEstudiantsFitxer {
+    public static void main(String[] args) {
+
+    }
+
+    static int[] topIndices(float[] notes, int k) {
+        int[] a = {3,5};
+        return a;
+    }
+}
