366402873bd0a5e8b93e2cb79c199908bd3c9adc
emittal <eshaan.mittal1@gmail.com>
2022-12-20 12:13:41 +0100

    8

--- src/MillorsEstudiants/MillorsEstudiantsFitxer.java
+++ src/MillorsEstudiants/MillorsEstudiantsFitxer.java
@@ -1,9 +1,6 @@
 package MillorsEstudiants;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class MillorsEstudiantsFitxer {
@@ -22,6 +19,13 @@ public class MillorsEstudiantsFitxer {
             grades = new float[num_top];
             String aux_name;
             float aux_grade;
+            /*
+              Lee el fichero hasta el final
+              Primero(): Primera linia del fichero
+              Siguente(): Linia++
+              Final(): Ultima linia
+              Recorregut
+             */
             while (file_reader.hasNext()) {
                 aux_name = file_reader.next();
                 aux_grade = file_reader.nextFloat();
@@ -38,6 +42,13 @@ public class MillorsEstudiantsFitxer {
             else {
                 FileWriter file_to_write = new FileWriter(file_name_write);
                 file_to_write.write("Els " + num_top + " estudiants amb millor nota:\n");
+                /*
+                  Escribe al fichero los mejores estudiantes y notas, numero determinado por fichero que se lee
+                  Primero(): Primer estudiante
+                  Siguente: estudiante++
+                  Final(): Ultimo estudiante
+                  Recoreegut
+                 */
                 for (int i = 0; i < num_top; i++)
                     file_to_write.write(names[i] + " " + grades[i] + "\n");
                 file_to_write.close();
@@ -52,12 +63,34 @@ public class MillorsEstudiantsFitxer {
         }
     }
 
+    /**
+     * Actualiza el array de nombres y notas si el nuevo estudiante es uno de los con mejores nota
+     * @param names Array de nombres con mejores nota
+     * @param grades Array de notas con mejores nota
+     * @param new_name Nombre de nuevo estudiante leido desde el fichero
+     * @param new_grade Nota de nuevo estudiante leido desde el fichero
+     * @param size Numero de estudiantes que queremos al final, tamaño del arrays
+     */
     static void millorsEstudiants(String[] names, float[] grades, String new_name, float new_grade, int size) {
         boolean not_replaced = true;
+        /*
+          Encuentra la posicion del nuevo estudiante
+          Primer(): primero de mejores
+          Siguente(): pos++ de mejores
+          Final(): nota no es mejor que los otros / nueva nota es mejor que posicion i
+          Recoregut / Cerca: Mejor que posicion i
+         */
         for (int i = 0; i < size && not_replaced; i++) {
             if (new_grade > grades[i]) {
                 String aux_name = new_name, aux2_name;
                 float aux_grade = new_grade, aux2_grade;
+                /*
+                  Actualiza los arrays para cambiar las posiciones
+                  Primer(): Primer estudiante que baja en ranking
+                  Siguente(): Estudiante++
+                  Final(): Ultima posicion ha sido actualizado
+                  Recorregut
+                 */
                 for (int j = i; j < size; j++) {
                     aux2_name = names[j]; aux2_grade = grades[j];
                     names[j] = aux_name; grades[j] = aux_grade;
